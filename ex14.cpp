e69c9b01baaa148a27e1f04f86c5ca2a2ef24aaa
samatluck <samatluck@gmail.com>
2019-12-07 22:05:53 -0600

    Add files via upload

--- ex14.cpp
+++ ex14.cpp
@@ -16,51 +16,51 @@ int main (int argc, char *argv[]) {
     int id = omp_get_thread_num();
 
     // for loop without 'nowait'
-#pragma omp single 
+//#pragma omp single
     {
       std::cout << "for loop\n";
     }
 
-#pragma omp for
+//#pragma omp for
     for (int i = 0 ; i < 100; i++){
       for (int j = 0 ; j < 1000000 * (id + 1); j ++){
 	// time weight
       }
     }
     double t1 = omp_get_wtime() - start;
-#pragma omp critical (t1)
+//#pragma omp critical (t1)
     {
       std::cout << "Thread" << id << " come here at " << t1 << std::endl;
     }
 
-#pragma omp barrier
+//#pragma omp barrier
     // for loop with 'nowait'    
-#pragma omp single 
+//#pragma omp single
     {
       std::cout << "for loop nowait\n";
     }
     start = omp_get_wtime();
-#pragma omp for nowait
+//#pragma omp for nowait
     for (int i = 0 ; i < 100; i++){
       for (int j = 0 ; j < 1000000 * (id + 1) ; j ++){
 	// time weight
       }
     }
     double t2 = omp_get_wtime() - start;
-#pragma omp critical (t2)
+//#pragma omp critical (t2)
     {
       std::cout << "Thread" << id << " come here at " << t2 << std::endl;
     }
 
-#pragma omp barrier
+//#pragma omp barrier
     // for loop with 'reduction' 'nowait'    
-#pragma omp single 
+//#pragma omp single
     {
       std::cout << "for loop nowait reduction\n";
     }
     start = omp_get_wtime();
     a = 0;
-#pragma omp for nowait reduction(+:a)
+//#pragma omp for nowait reduction(+:a)
     for (int i = 0 ; i < 100 ; i++){
       a += 1; 
       for (int j = 0 ; j < 1000000 * (id + 1) ; j ++){
@@ -68,12 +68,12 @@ int main (int argc, char *argv[]) {
       }
     }
     double t3 = omp_get_wtime() - start;
-#pragma omp critical (t3)
+//#pragma omp critical (t3)
     {
       std::cout << "Thread" << id << " come here at " << t3 << " a= " << a << std::endl;
     }
-#pragma omp barrier
-#pragma omp critical (last)
+//#pragma omp barrier
+//#pragma omp critical (last)
     {
       std::cout << "Thread" << id << " a= " << a << std::endl;
     }
