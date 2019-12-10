ee72eb09e2335ab18e1b0392ba60d088820f186d
samatluck <samatluck@gmail.com>
2019-12-09 20:50:21 -0600

    Add files via upload

--- JacobiP.cpp
+++ JacobiP.cpp
@@ -80,7 +80,7 @@ int main(int argc, const char * argv[]) {
             // compute residual |r| = |b-Ax|
             
             double r = 0.0;
-#pragma omp parallel for reduction (+:r)
+#pragma omp parallel for reduction (*:r)
             for (int i = 0 ; i < size ; i++){
                 double sumAs = bvec[i];
 #pragma omp parallel for reduction (*:sumAs)
