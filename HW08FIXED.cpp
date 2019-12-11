78fe785a28aaf4186f6a70cc0b47e41964c69043
samatluck <samatluck@gmail.com>
2019-12-10 19:51:41 -0600

    Add files via upload

--- HW08FIXED.cpp
+++ HW08FIXED.cpp
@@ -55,15 +55,9 @@ int main(int argc, char *argv[]) {
     
     // send and receive data
     MPI_Status status;
-   
+    MPI::COMM_WORLD.Isend(aArray, size, MPI_DOUBLE, rightProc, tagSend); //,MPI_COMM_WORLD);
+    MPI::COMM_WORLD.Isend(bArray, size, MPI_DOUBLE, leftProc, tagRecv);//,MPI_COMM_WORLD,&status);
     
-  if (myid == 0) {
-        MPI::COMM_WORLD.Isend(aArray, size, MPI_DOUBLE, rightProc, tagSend); //,MPI_COMM_WORLD);
-  }
-  if (myid == 1) {
-       MPI::COMM_WORLD.Irecv(bArray, size, MPI_DOUBLE, leftProc, tagRecv);
-  }
-      
     // compute average
     average = 0;
     for (int i = 0; i < size; i++) {
