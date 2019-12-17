b666cd91566615298e664d4ac6d6edf2cae58105
samatluck <samatluck@gmail.com>
2019-12-16 22:10:04 -0600

    Add files via upload

--- hw3n.cpp
+++ hw3n.cpp
@@ -78,7 +78,7 @@ int main(int argc, const char * argv[]) {
         // compute residual |r| = |b-Ax|
         double r = 0.0;
 #pragma omp end declare target
-#pragma omp parallel for private (i,j)
+#pragma omp for schedule(dynamic,10) collapse(2)
         for (int i = 0 ; i < size ; i++){
             double sumAs = bvec[i];
             for (int j = 0 ; j < size ; j++){
